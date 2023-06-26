682a23dde9ab7dc350a3dd877f73257541727c12
kevyn angelo montana <angelo@inqbyte.io>
2023-06-26 13:11:43 +0800

    added workspace pushing

--- docker-build.sh
+++ docker-build.sh
@@ -12,12 +12,16 @@ rm .temp
 #push to dockerhub
 echo "pushing to dockerhub"
 docker push "$ORGANIZATION/$COMPOSE_PROJECT_NAME-php-fpm:$TAG-$PLATFORM"
+docker push "$ORGANIZATION/$COMPOSE_PROJECT_NAME-workspace:$TAG-$PLATFORM"
 
 #create multiarch manifest
 echo "creating shared manifest"
 docker manifest create $ORGANIZATION/$COMPOSE_PROJECT_NAME-php-fpm:$TAG \
     $ORGANIZATION/$COMPOSE_PROJECT_NAME-php-fpm:$TAG-$PLATFORM
+docker manifest create $ORGANIZATION/$COMPOSE_PROJECT_NAME-php-fpm:$TAG \
+    $ORGANIZATION/$COMPOSE_PROJECT_NAME-workspace:$TAG-$PLATFORM
 
 #push to dockerhub
 echo "pushing shared manifest to dockerhub"
 docker manifest push --purge $ORGANIZATION/$COMPOSE_PROJECT_NAME-php-fpm:$TAG
+docker manifest push --purge $ORGANIZATION/$COMPOSE_PROJECT_NAME-workspace:$TAG
